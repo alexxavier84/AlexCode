22cb17b330003519f97715a57429a7e4532608e5
alexxavier84 <alexxavier84@users.noreply.github.com>
2023-11-19 13:37:32 +0000

    Home page and topbar section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
+import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
+import Topbar from "../components/Topbar/Topbar";
 
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className='bg-dark-layer-2 min-h-screen'>
+      <Topbar/>
+      <h1 className="text-2xl text-center text-gray-400 dark:text-gray-400 font-medium uppercase mt-10 mb-5">
+        Quality over Quantity
+      </h1>
+      <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+        <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
+          <thead className="text-xs text-gray-400 uppercase dark:text-gray-400 border-b">
+            <tr>
+              <th className="px-1 py-3 w-0 font-medium" scope="col">Status</th>
+              <th className="px-6 py-3 w-0 font-medium" scope="col">Title</th>
+              <th className="px-6 py-3 w-0 font-medium" scope="col">Difficulty</th>
+              <th className="px-6 py-3 w-0 font-medium" scope="col">Category</th>
+              <th className="px-6 py-3 w-0 font-medium" scope="col">Solution</th>
+            </tr>
+          </thead>
+          <ProblemsTable />
+        </table>
+      </div>
     </main>
   )
 }
